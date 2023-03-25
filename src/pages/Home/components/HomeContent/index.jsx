22f92428719158c4c64e0a0ef26f38708a387d48
James <james.hinton@live.co.uk>
2023-03-25 13:22:33 +0000

    style: fixed home style breakpoints

--- src/pages/Home/components/HomeContent/index.jsx
+++ src/pages/Home/components/HomeContent/index.jsx
@@ -7,7 +7,6 @@ const HomeContent = () => {
     <div className='home'>
       <Hero
         imageSrc='https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-        height='10em'
         children={
           <h1>
             Bespoke wedding ceremonies by{' '}
