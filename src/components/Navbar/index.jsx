c7d179027e8b25f8a56e78a8e0032d18c74f1c79
James <james.hinton@live.co.uk>
2023-03-25 10:32:07 +0000

    feat: added navbar links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,19 @@ import './style.scss'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src='logo.png' alt='logo' className='logo' />
+      <img src='logo.png' alt='logo' className='navbar__logo' />
+
+      <div className='navbar__links'>
+        <a href='#' className='navbar__link'>
+          Home
+        </a>
+        <a href='#' className='navbar__link'>
+          About
+        </a>
+        <a href='#' className='navbar__link'>
+          Contact
+        </a>
+      </div>
     </div>
   )
 }
